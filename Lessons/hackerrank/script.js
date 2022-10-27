93921a1d3252be06265ac763742b658203360b8a
Глеб Иванов Евгеньевич <@lcoast.itstep>
2022-10-27 21:59:49 +0600

    27-10-2022-2

--- Lessons/hackerrank/script.js
+++ Lessons/hackerrank/script.js
@@ -13,17 +13,80 @@
 
 // console.log(catAndMouse(1, 3, 2));
 
-function saveThePrisoner(np, ns, s) {
-    if (ns + (s - 1) > np) {
-        return ns + (s - 1) - np + 1;
-    }
-    if (ns < np - s) {
-        // s = s % ns;
-    }
-    // if (np < ns) {
-    //     ns = ns % np;
-    // }
-    return ns + (s - 1);
+// function saveThePrisoner(np, ns, s) {
+//     if (ns + (s - 1) > np) {
+//         return ns + (s - 1) - np + 1;
+//     }
+//     if (ns < np - s) {
+//         // s = s % ns;
+//     }
+//     // if (np < ns) {
+//     //     ns = ns % np;
+//     // }
+//     return ns + (s - 1);
+// }
+
+// console.log(saveThePrisoner(10, 7, 8)); unfinished
+
+// function circularArrayRotation(a, k, queries) {
+//     let result = [];
+
+//     queries.forEach((el) => {
+//         // console.log((el + k) % a.length);
+//         console.log((el + k) % a.length);
+//         // console.log(a.length);
+//         result.push(a[(el + k) % a.length]);
+//     });
+
+//     return result;
+// }
+
+// console.log(circularArrayRotation([1, 2, 3], 1, [0, 1, 2]));
+// unfinished
+
+// function beautifulDays(start, end, div) {
+//     const reverseInt = (num) => {
+//         return parseInt(num.toString().split("").reverse().join(""));
+//     };
+
+//     let res = 0;
+//     for (let i = start; i <= end; i++) {
+//         let reverse = reverseInt(i);
+//         if (Math.abs(i - reverse) % div === 0) {
+//             res++;
+//         }
+//     }
+
+//     return res;
+// }
+
+// console.log(beautifulDays(13, 45, 3));
+
+// function pageCount(end, num) {
+//     let a = num - 1;
+//     let b = end - num;
+
+//     if (num % 2 == 0) {
+//         if (b == 1) {
+//             b = 0;
+//         }
+//     }
+
+//     return Math.min(a, b);
+// }
+
+// console.log(pageCount(5, 3));
+
+// drawing-book unfinished
+
+function designerPdfViewer(array, word) {
+    let tallness = [];
+
+    word.split("").forEach((el, i) => {
+        tallness.push(array[word.charCodeAt(i) - 97]);
+    });
+
+    return Math.max(...tallness) * word.length;
 }
 
-console.log(saveThePrisoner(10, 7, 8));
+console.log(designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7], "zaba"));
